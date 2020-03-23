77886083abb57a8a2d295b06f379fe7037deef20
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2020-03-23 22:56:57 +0100

    Initial files

---
+++ conv.py
@@ -0,0 +1,43 @@
+from shapes_helper import ShapesHelper
+import numpy as np
+
+x = np.array([[1, 2, 3, 4],
+              [2, 1, 6, 6],
+	          [1, 5, 4, 3],
+              [5, 4, 8, 2]])
+
+f = np.array([[2, 3],
+              [1, 1]])
+
+stride = 2
+
+helper = ShapesHelper(x, f, stride)
+
+# get all the parameters
+unique_positions, x_top, x_bottom, y_top, y_bottom, \
+                  x_rows, x_cols, f_rows, f_cols = helper.get_all_params()
+
+# initialize slices
+slices = []
+slices.append(x[y_top : y_bottom, x_top: x_bottom])
+
+for i in range(0, unique_positions-1):
+    x_top += stride
+    x_bottom += stride
+
+    position = x[y_top : y_bottom, x_top: x_bottom]
+    if position.size == 0 or position.size != f_rows*f_cols:
+        x_top = 0
+        x_bottom = f_cols
+        y_top += stride
+        y_bottom += stride
+
+        #print(f'x[{y_top}:{y_bottom}, {x_top}:{x_bottom}]')
+
+        position = x[y_top : y_bottom, x_top: x_bottom]
+    
+    slices.append(position)
+
+for position in slices:
+    print(position)
+    output = np.vdot(position, f)
---
+++ shapes_helper.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+class ShapesHelper():
+    def __init__(self, x, f, stride):
+        self.x = x # image
+        self.f = f # filter
+        self.stride = stride
+
+        self.initialize_shapes()
+        self.unique_positions = self.get_unique_positions()
+
+    def initialize_shapes(self):
+        self.f_rows, self.f_cols = self.f.shape
+        self.x_rows, self.x_cols = self.x.shape
+
+        # only accept the size N=M
+        assert self.f_rows == self.f_cols
+        assert self.x_rows == self.x_cols
+
+        # matrix coordinates
+        self.x_top, self.x_bottom = 0, self.f_cols
+        self.y_top, self.y_bottom = 0, self.f_rows
+
+    def get_unique_positions(self):
+        x_positions = self._get_num_x_positions()
+        y_positions = self._get_num_y_positions()
+
+        return x_positions * y_positions
+
+    def get_all_params(self):
+        return self.unique_positions, self.x_top, self.x_bottom, self.y_top, self.y_bottom, \
+               self.x_rows, self.x_cols, self.f_rows, self.f_cols
+
+    def _get_num_x_positions(self):
+        num_positions_x = 0
+
+        for i in range(self.x_cols):
+            num_positions_x += 1
+
+            self.x_top += self.stride
+            self.x_bottom += self.stride
+
+            position = self.x[self.y_top : self.y_bottom, self.x_top: self.x_bottom]
+            if position.size == 0 or position.size != self.f_rows*self.f_cols:
+                # reset and stop loop
+                self.initialize_shapes()
+                break
+
+        return num_positions_x
+
+    def _get_num_y_positions(self):
+        num_positions_y = 0
+
+        for i in range(self.x_rows):
+            num_positions_y += 1
+
+            self.y_top += self.stride
+            self.y_bottom += self.stride
+
+            position = self.x[self.y_top : self.y_bottom, self.x_top: self.x_bottom]
+            if position.size == 0 or position.size != self.f_rows*self.f_cols:
+                # reset and stop loop
+                self.initialize_shapes()
+                break
+
+        return num_positions_y
